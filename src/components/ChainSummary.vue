e943ef5955a221b532ab72c1830fc49ed107ee80
Rahmad Ardi Handoyo <158312970+Pilex3173@users.noreply.github.com>
2025-07-19 15:48:09 +0700

    Update ChainSummary.vue

--- src/components/ChainSummary.vue
+++ src/components/ChainSummary.vue
@@ -4,42 +4,49 @@ import { computed } from 'vue';
 import { Icon } from '@iconify/vue';
 
 const props = defineProps({
-  name: {
-    type: String,
-    required: true,
-  },
+  name: {
+    type: String,
+    required: true,
+  },
 });
 
 const dashboardStore = useDashboard();
 const conf = computed(() => dashboardStore.chains[props.name] || {});
 
 const addFavor = (e: Event) => {
-  e.stopPropagation();
-  e.preventDefault();
-  dashboardStore.favoriteMap[props.name] = !dashboardStore?.favoriteMap?.[props.name];
-  window.localStorage.setItem('favoriteMap', JSON.stringify(dashboardStore.favoriteMap));
+  e.stopPropagation();
+  e.preventDefault();
+  dashboardStore.favoriteMap[props.name] = !dashboardStore?.favoriteMap?.[props.name];
+  window.localStorage.setItem('favoriteMap', JSON.stringify(dashboardStore.favoriteMap));
 };
 </script>
+
 <template>
-  <RouterLink
-    :to="`/${name}`"
-    class="bg-base-100 hover:bg-gray-100 dark:hover:bg-[#373f59] rounded shadow flex items-center px-3 py-3 cursor-pointer"
-  >
-    <div class="w-8 h-8 rounded-full overflow-hidden">
-      <img :src="conf.logo" />
-    </div>
-    <div class="font-semibold ml-4 text-base flex-1 truncate capitalize">
-      {{ conf?.prettyName || props.name }}
-    </div>
-    <div
-      @click="addFavor"
-      class="pl-4 text-xl"
-      :class="{
-        'text-warning': dashboardStore?.favoriteMap?.[props.name],
-        'text-gray-300 dark:text-gray-500': !dashboardStore?.favoriteMap?.[props.name],
-      }"
-    >
-      <Icon icon="mdi-star" />
-    </div>
-  </RouterLink>
+  <RouterLink
+    :to="`/${name}`"
+    class="bg-base-100 hover:bg-gray-100 dark:hover:bg-[#373f59] rounded shadow flex items-center px-3 py-3 cursor-pointer"
+  >
+    <!-- Tampilkan logo hanya jika ada -->
+    <div class="w-8 h-8 rounded-full overflow-hidden bg-white flex items-center justify-center">
+      <img v-if="conf.logo" :src="conf.logo" :alt="conf.prettyName || name" class="w-full h-full object-contain" />
+      <Icon v-else icon="mdi:image-off" class="text-gray-400 text-xl" />
+    </div>
+
+    <!-- Nama Chain -->
+    <div class="font-semibold ml-4 text-base flex-1 truncate capitalize">
+      {{ conf?.prettyName || props.name }}
+    </div>
+
+    <!-- Favorite Toggle -->
+    <div
+      @click="addFavor"
+      class="pl-4 text-xl"
+      :class="{
+        'text-warning': dashboardStore?.favoriteMap?.[props.name],
+        'text-gray-300 dark:text-gray-500': !dashboardStore?.favoriteMap?.[props.name],
+      }"
+    >
+      <Icon icon="mdi-star" />
+    </div>
+  </RouterLink>
 </template>
